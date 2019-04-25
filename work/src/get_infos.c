831b77a2eb3f2f21ca183d3d80938495c9d147a0
Mr-rokma <lahcen.saidi88@gmail.com>
2019-04-25 19:08:34 +0100

    update

--- work/src/get_infos.c
+++ work/src/get_infos.c
@@ -29,7 +29,7 @@ void		get_plat(t_filler *fr)
 
 	if (ft_get_next_line(0, &fr->line))
 		free(fr->line);
-	fr->pla = (char **)malloc(sizeof(char *) * fr->p_h + 1);
+	fr->pla = (char **)malloc(sizeof(char *) * (fr->p_h + 1));
 	fr->pla[fr->p_h] = 0;
 	i = -1;
 	while (++i < fr->p_h)
@@ -38,7 +38,7 @@ void		get_plat(t_filler *fr)
 		ft_get_next_line(0, &fr->line);
 		fr->temp = fr->line;
 		fr->line = ft_strstr(fr->line, (fr->line + 4));
-		fr->pla[i] = (char *)malloc(sizeof(char) * fr->p_w + 1);
+		fr->pla[i] = (char *)malloc(sizeof(char) * (fr->p_w + 1));
 		fr->pla[i][fr->p_w] = '\0';
 		while (++j < fr->p_w)
 			fr->pla[i][j] = fr->line[j];
@@ -62,13 +62,13 @@ void		get_token(t_filler *fr)
 	int		j;
 
 	i = -1;
-	fr->token = (char **)malloc(sizeof(char *) * fr->t_h + 1);
+	fr->token = (char **)malloc(sizeof(char *) * (fr->t_h + 1));
 	fr->token[fr->t_h] = NULL;
 	while (++i < fr->t_h)
 	{
 		j = -1;
 		ft_get_next_line(0, &fr->line);
-		fr->token[i] = (char *)malloc(sizeof(char *) * fr->t_w + 1);
+		fr->token[i] = (char *)malloc(sizeof(char) * (fr->t_w + 1));
 		fr->token[i][fr->t_w] = '\0';
 		while (++j < fr->t_w)
 			fr->token[i][j] = fr->line[j];
